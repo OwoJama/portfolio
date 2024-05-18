d51a255cc9e26af4b5c8f4472aeb48677a3b0507
OwoJama <jamestinuola007@gmail.com>
2024-05-18 03:47:02 +0100

    new commit

--- portfolio_app/tests.py
+++ portfolio_app/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+
+class HomeTestPage(SimpleTestCase):
+    def test_home_url(self):
+        form = self.client.get('/')
+        self.assertEqual(form.status_code, 200)
+
+    def test_home_reverse(self):
+        form = self.client.get(reverse('index'))
+        self.assertEqual(form.status_code, 200)
